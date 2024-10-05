439200827db79ad1116589ac5b2f73b56f5b2545
Agnieszka Szczepanik <agnieszkaszczepanik93@gmail.com>
2024-10-05 10:58:55 +0200

    css

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
+
+const API_KEY = '46345761-d1dca7c8b8491e7477b3f2314';
+const API_URL = 'https://pixabay.com/api/';
